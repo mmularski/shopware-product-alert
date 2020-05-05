3c34a9c9b568ed6d806ac4198e0ee13d5cab7f9e
mmularski <mmularczyk9@gmail.com>
2020-05-05 11:02:54 +0200

    service skeleton

---
+++ src/SalesChannel/ProductAlertService.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @package  shopware_dev
+ * @author Marek Mularczyk <mmularczyk@divante.pl>
+ * @copyright 2020 Divante Sp. z o.o.
+ * @license See LICENSE_DIVANTE.txt for license details.
+ */
+
+/**
+ * Class ProductAlertService
+ */
+class ProductAlertService
+{
+
+}
