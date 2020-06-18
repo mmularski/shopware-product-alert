98539fb409ca782e7e43ddefcd589c71cc889f47
mmularski <mmulaczyk9@gmail.com>
2020-06-18 11:16:22 +0200

    Marketplace preparation - part 1

---
+++ src/DivanteProductAlert.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @package Divante\ProductAlert
+ * @author Marek Mularczyk <mmularczyk@divante.pl>
+ * @copyright 2020 Divante Sp. z o.o.
+ * @license See LICENSE_DIVANTE.txt for license details.
+ */
+
+namespace Divante\ProductAlert;
+
+use Shopware\Core\Framework\Plugin;
+
+class ProductAlert extends Plugin
+{
+}
